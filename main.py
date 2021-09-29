9301459586491b702e84461aa6333862a30ac9e2
maddieiverson22 <825522@mystma.org>
2021-09-29 14:20:03 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
+
+def password(p):
+    if p == "Knights19":
+        return "access granted"
+    else:
+        return "access denied"
+print (password("Knights19"))
+print (password("blahblahblah)"))
